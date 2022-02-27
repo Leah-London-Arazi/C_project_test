6a97975afe19fa3ca95addf7a6035562bde8dcb3
Leah-London-Arazi <leahl@mail.tau.ac.il>
2022-02-27 18:20:44 +0200

    first tests version

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 before everything - open cmd and 
 1. clone project.
 2. open spkmeans_compare.py.
-3. change FINAL_PROJECT_PATH to be your final project path that includes the updated build dir.
+3. change FINAL_PROJECT_PATH to be your final project path that includes the spkmeans.py file.
 4. change TEST_PROJECT_PATH to be your repository path.
 5. make sure that your python module name is "mykmeanssp".
-6. open cmd from the repository path.
-7. run "python spkmeans_compare.py"
+6. copy the module build to the repository path.
+7. open cmd from the repository path.
+8. run "python spkmeans_compare.py"
---
+++ spkmeans_compare.py
@@ -0,0 +1,44 @@
+import os, sys, json
+from mykmeanssp import *
+
+FINAL_PROJECT_PATH = r'C:\Users\Leah London Arazi\Dropbox\My PC (Leah-X1-Carbon)\Documents\Leah X1 Carbon\university\2022a\program project\C_project\final_project'
+TEST_PROJECT_PATH = r'C:\Users\Leah London Arazi\Dropbox\My PC (Leah-X1-Carbon)\Documents\Leah X1 Carbon\university\2022a\program project\C_project_test'
+INPUTS_PATH = os.path.join(TEST_PROJECT_PATH, "tests_inputs_outputs\\inputs")
+RESULTS_PATH = os.path.join(TEST_PROJECT_PATH, "tests_inputs_outputs\\outputs")
+YOUR_RESULTS_PATH = os.path.join(TEST_PROJECT_PATH, "your_outputs")
+COMPARE_JSON_FILE_PATH = os.path.join(TEST_PROJECT_PATH, "compare.json")
+
+def compare_outputs_python(outputs_dir, inputs_dir):
+    # load json data
+    with open(COMPARE_JSON_FILE_PATH, "r") as f:
+        json_data = json.load(f)
+
+    # change current dir
+    os.chdir(FINAL_PROJECT_PATH)
+
+    for test_file in json_data:
+        input_file_name = test_file["input_file_name"]
+        input_file_path = os.path.join(INPUTS_PATH, input_file_name)
+        tests = test_file["tests"]
+        for test in tests:
+            command_no_path = test["command"]
+            output_file_relative_path = test["output_file_relative_path"]
+            output_file_path = os.path.join(YOUR_RESULTS_PATH, output_file_relative_path)
+            command = command_no_path.format(input_file_path)
+            os.system('{} > "{}"'.format(command, output_file_path))
+
+            # compare results
+            with open(output_file_path, "r") as f:
+                your_res = f.read()
+            with open(os.path.join(RESULTS_PATH, output_file_relative_path), "r") as f:
+                our_res = f.read()
+            if (your_res != our_res):
+                print("### X ### the result of the command: {}, is not identical!".format(command))
+            else:
+                print("### V ### the result of the command: {}, is identical.".format(command))
+
+def main():
+    compare_outputs_python(YOUR_RESULTS_PATH, INPUTS_PATH)
+
+if __name__ == "__main__":
+    main()
